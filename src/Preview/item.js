d24caf87b1df08543370046285e6e9116b1850c5
Manuel Scarapazzi <manuelfrulloscarapazzi@gmail.com>
2018-01-15 21:27:33 +0100

    initial commit

--- src/Preview/item.js
+++ src/Preview/item.js
@@ -3,28 +3,45 @@ import '../index.css'
 import Radium from 'radium'
 
 class Item extends Component {
-  // const list = "<h2>Ciao</h2>"
   constructor(props) {
     super(props);
   }
 
   render() {
-    var hr = {
-      'color':'grey',
-      'background-color': 'grey'
-    }
     var item = {
-      'align-items':'center',
+      'align-items': 'center',
       'padding': '10 px',
-      'height': '35px',
       'list-style-type': 'none',
       cursor: 'pointer',
       'width': 'auto',
       color: '#000000',
-      'font-size':'16px',
-      'font-weight':'normal',
-      'text-align':'left',
-      'text-size-adjust':'100%'
+      'font-size': '16px',
+      'font-weight': 'normal',
+      'text-align': 'left',
+      'text-size-adjust': '100%'
+    }
+    var hr = {
+      'color': 'grey',
+      'background-color': 'grey'
+    }
+    if (this.props.content.complete.length > 48) {
+      item.push(  
+        'height': '15%'
+      )
+    } else {
+      item = {
+        'align-items': 'center',
+        'padding': '10 px',
+        'height': '10%',
+        'list-style-type': 'none',
+        cursor: 'pointer',
+        'width': 'auto',
+        color: '#000000',
+        'font-size': '16px',
+        'font-weight': 'normal',
+        'text-align': 'left',
+        'text-size-adjust': '100%'
+      }
     }
 
     return (<li style={item} onClick={() => {
@@ -32,7 +49,7 @@ class Item extends Component {
         actions.hideWindow()
       }}>
       {this.props.content.complete}
-    <hr style={hr}/>
+      <hr style={hr}/>
     </li>)
   }
 }
